7a44949f0727f1965d79adaebe56e13fb35681f4
hugocvx <hugo@yar.eu>
2020-01-30 23:37:28 +0100

    Update jquery.fittext.js

--- jquery.fittext.js
+++ jquery.fittext.js
@@ -17,24 +17,38 @@
     var compressor = kompressor || 1,
         settings = $.extend({
           'minFontSize' : Number.NEGATIVE_INFINITY,
-          'maxFontSize' : Number.POSITIVE_INFINITY
-        }, options);
+          'maxFontSize' : Number.POSITIVE_INFINITY,
+          callback : function{}
+         }, options);
 
     return this.each(function(){
 
       // Store the object
-      var $this = $(this);
+      var $this = $(this), 
+          wlength = $this.text().length || 1,
+          size = Math.min(
+                    $this.width() / wlength / compressor, 
+                    $this.innerHeight() / compressor, 
+                    parseFloat(settings.maxFontSize)
+            );
+      
 
-      // Resizer() resizes items based on the object width divided by the compressor * 10
+      // Resizer() resizes items based on the object width divided by 
+      // total character found in the element and divided again with the total paragraph line
+      // or using object height divided by paragraph line
       var resizer = function () {
-        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+        
+        $this.css('font-size', Math.max(size,parseFloat(settings.minFontSize)));
+        
+        callback() ;
       };
 
       // Call once to set.
       resizer();
 
       // Call on resize. Opera debounces their resize by default.
-      $(window).on('resize.fittext orientationchange.fittext', resizer);
+      $(window)
+        .on('resize.fittext orientationchange.fittext', resizer);
 
     });
 
